baf71f3e523e012d182f4683eec59ec221aa7b66
Brian <brianfransis.moniaga@gmail.com>
2024-11-20 11:25:30 -0700

    a little fun.

---
+++ write_program.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+// A function that creates an is_even.cpp
+void create_is_even_program() {
+    const int MAX_DEPTH = 10000;
+
+    std::ofstream file;
+    file.open("is_even.cpp");
+    file << "bool is_even(int x) {\n";
+    file << "  switch (x) {\n";
+    for (int i = 0; i <= MAX_DEPTH; i++) {
+        file << "    case " << i << ":\n";
+        if (i%2 == 0)
+            file << "      return true;\n";
+        else
+            file << "      return false;\n";
+    }
+    file << "    default:\n";
+    file << "      return (x&1) == 0;\n";
+    file << "  }\n";
+    file << "}\n";
+    file.close();
+}
+
+int main() {
+    create_is_even_program();
+}
